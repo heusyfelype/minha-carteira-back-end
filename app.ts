3630a2a87415dcc0ef9dda81cc37729f9262af2a
heusyfelype <felype.heusy@gmail.com>
2023-10-07 15:54:45 -0300

    release: iniciando back end do 'minha certeira'

---
+++ app.ts
@@ -0,0 +1,15 @@
+import cors from "cors";
+import express from "express";
+import "express-async-errors";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+//app.use(router);
+//app.use(handleErrors);
+
+export default app;
